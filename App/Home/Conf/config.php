3799da86774439df7915023731dc2b31bedda22c
xialeizhou <xialeizhou@wentouonline.com>
2015-01-07 10:43:00 +0800

    投标bug修复

--- App/Home/Conf/config.php
+++ App/Home/Conf/config.php
@@ -2,11 +2,11 @@
 return array(
     /* 数据库设置 */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  '182.92.231.29', // 服务器地址
-    'DB_NAME'               =>  'dict',          // 数据库名
-    'DB_USER'               =>  'wt_admin',      // 用户名
-    'DB_PWD'                =>  '1qaz@WSX',          // 密码
-    'DB_PORT'               =>  '3306',        // 端口
+    'DB_HOST'               =>  '', // 服务器地址
+    'DB_NAME'               =>  '',          // 数据库名
+    'DB_USER'               =>  '',      // 用户名
+    'DB_PWD'                =>  '',          // 密码
+    'DB_PORT'               =>  '',        // 端口
     'DB_PREFIX'             =>  '',    // 数据库表前缀
     'DB_PARAMS'	            =>  array(), // 数据库连接参数
     'DB_DEBUG'		    =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
